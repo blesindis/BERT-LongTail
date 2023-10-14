3f51bbe5a8ffd49c95366a72de008c26cf433914
Mengyi <bleschen@outlook.com>
2023-10-14 09:42:32 +0800

    Code Refinement: delete some useless code and add some annotations

--- pretrain_stage0.py
+++ pretrain_stage0.py
@@ -18,18 +18,6 @@ def set_seed(seed: int) -> None:
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
-    
-    
-def get_available_cuda_device() -> int:
-    max_devs = torch.cuda.device_count()
-    for i in range(max_devs):
-        try:
-            mem = torch.cuda.mem_get_info(i)
-        except:
-            continue
-        if mem[0] / mem[1] > 0.85:
-            return i
-    return -1
 
 
 def validate(model, val_loader, accelerator, device):
@@ -46,20 +34,15 @@ def validate(model, val_loader, accelerator, device):
     return perplexity
 
 
-def get_gradient_norms(model):
-    """Utility function to get gradient norms of a model."""
-    return [param.grad.norm().item() for param in model.parameters() if param.grad is not None]
-
-
 def train(model, num_epochs, dataset, device):
     # train_loader, val_loader, test_loader = dataset.train_loader, dataset.val_loader, dataset.test_loader
     train_loader, val_loader = dataset.train_loader, dataset.val_loader
-    optimizer = optim.AdamW(model.parameters(), lr=1e-5, weight_decay=0.01, betas=[0.9, 0.999], eps=1e-6)
+    optimizer = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0, betas=[0.9, 0.999], eps=1e-6)
     accelerator = Accelerator()
     writer = SummaryWriter("log/" + 'bert')
     
     num_updates = num_epochs * len(train_loader)
-    lr_scheduler = get_cosine_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=num_updates * 0.1, num_training_steps=num_updates)
+    lr_scheduler = get_cosine_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=num_updates * 0.06, num_training_steps=num_updates)
     # model, optimizer, lr_scheduler, train_loader, val_loader, test_loader = accelerator.prepare(model, optimizer, lr_scheduler, train_loader, val_loader, test_loader)
     model, optimizer, lr_scheduler, train_loader, val_loader = accelerator.prepare(model, optimizer, lr_scheduler, train_loader, val_loader)
     model.to(device)
@@ -73,7 +56,8 @@ def train(model, num_epochs, dataset, device):
             # print(next(model.parameters()).device)
             # for key, tensor in batch.items():
             #     print(f"{key} is on {tensor.device}")
-            loss, _, layer_outputs = model(**batch)
+            # loss, _, layer_outputs = model(**batch)
+            loss, _, _= model(**batch)
             losses.append(accelerator.gather(loss.repeat(config.batch_size)))
             
             
@@ -94,7 +78,7 @@ def train(model, num_epochs, dataset, device):
             # writer.add_scalar('perplexity_test', loss_test, epoch)
             writer.add_scalar('learning_rate', optimizer.param_groups[-1]['lr'], epoch)
         
-    accelerator.save_state('./output-formal-1-bs32-ep50-lr1e5')
+    accelerator.save_state('./output-0-savedecoder')
     
 
 if __name__ == "__main__":
@@ -103,9 +87,9 @@ if __name__ == "__main__":
     config = BertConfig.from_json_file('config/bert.json')
     dataset = RestaurantForLM_small(config=config)
     
-    device = torch.device("cuda:3") 
+    device = 'cuda'
     model = base_models.BertWithSavers(config=config)
     model.to(device)
     # model = nn.DataParallel(model)
     
-    train(model=model, num_epochs=50, dataset=dataset, device=device)
+    train(model=model, num_epochs=10, dataset=dataset, device=device)
