20e08d3cc38df4168e8a685bc321dcb4d1cfa5a7
Mengyi <bleschen@outlook.com>
2023-10-19 15:08:28 +0800

    Code Refinement

--- pretrain_stage1.py
+++ pretrain_stage1.py
@@ -1,17 +1,21 @@
+import os
+import random
+import argparse
+import numpy as np
+import torch
 import torch.nn as nn
-import base_models
-from transformers import BertConfig
-from Dataset import ACLForLM_small, RestaurantForLM_small
-from Dataset import Wikitext
+import torch.optim as optim
 from accelerate import Accelerator
 from torch.utils.tensorboard import SummaryWriter
 from transformers import BertConfig, get_cosine_schedule_with_warmup
-import torch.optim as optim
 
-import os
-import torch
-import numpy as np
-import random
+# Local imports
+import base_models
+from Dataset import ACLForLM_small, RestaurantForLM_small, Wikitext
+
+
+# Constants and Configurations
+SEED = 45
 
 
 def set_seed(seed: int) -> None:
@@ -20,6 +24,21 @@ def set_seed(seed: int) -> None:
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
+
+
+def parse_arguments():
+    """Parse arguments from command line."""
+    parser = argparse.ArgumentParser(description="Training script for BERT model.")
+    
+    # Add arguments
+    parser.add_argument('--num_epochs', type=int, default=50, help='Number of training epochs.')
+    parser.add_argument('--load_path', type=str, default="./output-0-saveall-1016", help='Path to load model parameters.')
+    parser.add_argument('--config_path', type=str, default='config/bert.json', help='Path to BERT config file.')
+    parser.add_argument('--train_on_newdata', action='store_true', help='Flag to train on new data.')
+    parser.add_argument('--replay_layerwise', action='store_true', help='Flag to replay layer-wise.')
+    parser.add_argument('--replay_decoder', action='store_true', help='Flag to replay in the decoder layer.')
+    
+    return parser.parse_args()
 
 
 def validate(model, val_loader, accelerator):
@@ -55,14 +74,16 @@ def batch_to_cpu(batch):
 
 def train(
     # model/data params
-    model=None, 
-    num_epochs=10, 
-    dataset=None, 
-    dataset_pre=None,
-    train_on_newdata=True,
-    replay_layerwise=True,
-    replay_decoder=False,
-    load_path=None
+    model, 
+    num_epochs, 
+    vocab_size,
+    batch_size,
+    dataset, 
+    dataset_pre,
+    load_path,
+    train_on_newdata,
+    replay_layerwise,
+    replay_decoder
 ):
     train_loader, val_loader = dataset.train_loader, dataset.val_loader
     pre_test_loader = dataset_pre.val_loader
@@ -107,7 +128,7 @@ def train(
                 batch = batch_to_cuda(batch)
                 
                 loss, _, _ = model(**batch)
-                losses.append(accelerator.gather(loss.repeat(config.batch_size)))
+                losses.append(accelerator.gather(loss.repeat(batch_size)))
                 
                 optimizer.zero_grad()
                 accelerator.backward(loss)
@@ -148,7 +169,7 @@ def train(
                     scores = model.head(output)
                     
                     label = layer_labels[l].to('cuda')
-                    mlm_loss = ce_loss(scores.view(-1, config.vocab_size), label.view(-1))
+                    mlm_loss = ce_loss(scores.view(-1, vocab_size), label.view(-1))
                     
                     label = label.to('cpu')
                     
@@ -205,17 +226,32 @@ def train(
     accelerator.save_state('./output-formal-2')
     
 
-if __name__ == "__main__":
-    set_seed(45)
+def main():
+    set_seed(SEED)
     
-    config = BertConfig.from_json_file('config/bert.json')
+    # Parse arguments
+    args = parse_arguments()
+    
+    config = BertConfig.from_json_file(args.config_path)
     dataset = ACLForLM_small(config=config)
     dataset_pre = RestaurantForLM_small(config=config)
     
     model = base_models.BertWithSavers(config=config)
     model.to('cuda')
-    # model = nn.DataParallel(model)
     
-    load_path = "./output-0-saveall-1016"
-    
-    train(model=model, num_epochs=50, dataset=dataset, dataset_pre=dataset_pre, load_path=load_path)
+    train(
+        model=model, 
+        num_epochs=args.num_epochs, 
+        vocab_size = config.vocab_size,
+        batch_size = config.batch_size,
+        dataset=dataset, 
+        dataset_pre=dataset_pre, 
+        load_path=args.load_path, 
+        train_on_newdata=args.train_on_newdata, 
+        replay_layerwise=args.replay_layerwise, 
+        replay_decoder=args.replay_decoder
+    )
+
+
+if __name__ == "__main__":
+    main()
