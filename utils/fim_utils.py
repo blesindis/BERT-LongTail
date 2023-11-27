72cfcbe56eafbdb6e22a5cfd355372b03dad229c
Mengyi <bleschen@outlook.com>
2023-11-27 13:50:32 +0800

    New Func: pretrain a vanilla bert(bert-stage1) & pretrain a vanilla bert with 2 datasets while monitor the parameter overlapping of 2 datasets in each layer(bert-joint)

---
+++ utils/fim_utils.py
@@ -0,0 +1,65 @@
+import torch
+from torch.nn.functional import cosine_similarity
+
+
+def compute_fim(model, dataloader, component='embedding'):
+    model.eval()
+    
+    component_fim = None
+
+    for i, batch in enumerate(dataloader):
+        model.zero_grad()
+        loss, _ = model(**batch)
+        loss.backward() 
+        
+        if component == 'embedding':
+            fim = torch.cat([p.grad.view(-1)**2 for p in model.bert.embeddings.parameters() if p.grad is not None])
+        elif component == 'decoder':
+            fim = torch.cat([p.grad.view(-1)**2 for p in model.head.parameters() if p.grad is not None])
+            
+        trace_fim = torch.sum(fim)
+        normalized_fim = fim / trace_fim
+        
+        if i == 0:
+            component_fim = normalized_fim
+        else:
+            component_fim += normalized_fim
+            
+    return component_fim
+            
+
+def compute_layer_fim(model, dataloader, num_layers):    
+    layer_fims = []
+    
+    model.eval()
+    
+    for i, batch in enumerate(dataloader):
+        model.zero_grad()
+        loss, _ = model(**batch)
+        loss.backward() 
+        
+        for j in range(num_layers):
+            gradients = torch.cat([p.grad.view(-1)**2 for p in model.bert.encoders.layers[j].parameters() if p.grad is not None])
+            fim = gradients
+            trace_fim = torch.sum(fim)
+            normalized_fim = fim / trace_fim
+            
+            if i == 0:
+                layer_fims.append(normalized_fim)
+            else:
+                layer_fims[j] += normalized_fim
+            
+    layer_fims = [fim / len(dataloader) for fim in layer_fims]
+        
+    return layer_fims
+
+
+def calculate_overlap(fim1, fim2):
+    # Ensure FIMs are on the same device and are 1-D tensors
+    fim1 = fim1.view(-1).to('cuda')
+    fim2 = fim2.view(-1).to('cuda')
+    
+    # Calculate cosine similarity between the FIMs
+    similarity = cosine_similarity(fim1.unsqueeze(0), fim2.unsqueeze(0))
+    return similarity
+    
