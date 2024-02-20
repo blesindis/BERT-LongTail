14be3c2ee5920eaa4508f22a029a027784fcc1c6
Mengyi <bleschen@outlook.com>
2024-02-20 12:35:13 +0800

    Cleansing: Delete old stage files

--- base_models.py
+++ base_models.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
 from transformer.Transformer import BertModel, BertMOEModel, SwitchBertModel
-from transformer.MoMoE import BertMoMoEModel
 from transformer.MoMoShare import BertMoMoShareModel
-from transformer.MoMoShareFix import BertMoMoShareFixModel
 from transformer.MoMoShareSwitch import BertMoMoShareSwitchModel
 from transformers.models.bert.modeling_bert import BertOnlyMLMHead
 
@@ -97,38 +95,8 @@ class BertWithMoMoShare(nn.Module):
         mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
 
         return mlm_loss, scores
-    
-    
-class BertWithMoMoShareFix(nn.Module):
-    def __init__(self, config, centers):
-        super(BertWithMoMoShareFix, self).__init__()
-        self.config = config        
-        self.bert = BertMoMoShareFixModel(config, centers)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
 
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
 
-    def forward(self, input_ids, attention_mask, labels, expert_index):
-        output = self.bert(input_ids, attention_mask, expert_index)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
-
-        return mlm_loss, scores
-    
-    
 class BertWithMoMoShareSwitch(nn.Module):
     def __init__(self, config):
         super(BertWithMoMoShareSwitch, self).__init__()
@@ -153,36 +121,6 @@ class BertWithMoMoShareSwitch(nn.Module):
 
     def forward(self, input_ids, attention_mask, labels, expert_index):
         output = self.bert(input_ids, attention_mask, expert_index)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
-
-        return mlm_loss, scores
-    
-    
-class BertWithMoMoE(nn.Module):
-    def __init__(self, config, centers, ffn_centers):
-        super(BertWithMoMoE, self).__init__()
-        self.config = config        
-        self.bert = BertMoMoEModel(config, centers, ffn_centers)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def forward(self, input_ids, attention_mask, labels):
-        output = self.bert(input_ids, attention_mask)
         scores = self.head(output)
         mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
 
