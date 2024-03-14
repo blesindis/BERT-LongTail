77938ee6fcea2f6d1f653732b5ad192c94e612d5
Mengyi <bleschen@outlook.com>
2024-03-14 09:53:54 +0800

    Cleansing

--- base_models.py
+++ base_models.py
@@ -3,19 +3,11 @@ import torch
 import torch.nn as nn
 from transformer.Transformer import BertModel
 from transformer.Switch import SwitchBertModel
-from transformer.SwitchDS import SwitchDSModel
 from transformer.MoMoShareSwitch import BertMoMoShareSwitchModel
 from transformer.MoMoSwitchCommon import BertMoMoSwitchCommonModel
-from transformer.MoMoShare import BertMoMoShareModel
-from transformer.MoMoShareCommonAttnLargeNew import BertMoMoShareCommonAttnLargeNewModel
-from transformer.MoMoDynamicCenter import BertMoMoDynamicCenterModel
-from transformer.MoMoDynamicDistribution import BertMoMoDynamicDistributionModel
 from transformer.MoMoModelRouter import BertMoMoModelRouterModel
-from transformer.MoMoModelRouterCommonLinGate import BertMoMoModelRouterCommonLinGateModel
 from transformer.MoMoModelRouterCommonLargeNew import BertMoMoModelRouterCommonAttnLargeNewModel
 from transformer.MoMoEncoderGating import BertMoMoEncoderGatingModel
-from transformer.MoMoEncoderGatingMCR import BertMoMoEncoderGatingMCRModel
-from transformer.MoMoAutoEncoderGating import BertMoMoAutoEncoderGatingModel
 from transformer.MoMoT import BertMoMoTModel
 from transformer.MoMoTSwitch import BertMoMoTSwitchModel
 from transformer.MoMoTSwitchEmb import BertMoMoTSwitchEmbModel
@@ -88,165 +80,7 @@ class BertSwitch(nn.Module):
         mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
 
         return mlm_loss, scores  
-    
-    
-class SwitchDS(nn.Module):
-    def __init__(self, config):
-        super(SwitchDS, self).__init__()
-        self.config = config
-        self.bert = SwitchDSModel(config)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
 
-    def _init_weights(self, module):
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-
-    def forward(self, input_ids, attention_mask, labels):
-        output = self.bert(input_ids, attention_mask)
-        output = self.bert(input_ids, attention_mask)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
-
-        return mlm_loss, scores  
-    
-    
-class BertWithMoMoShare(nn.Module):
-    def __init__(self, config, centers):
-        super(BertWithMoMoShare, self).__init__()
-        self.config = config        
-        self.bert = BertMoMoShareModel(config, centers)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def forward(self, input_ids, attention_mask, labels):
-        output = self.bert(input_ids, attention_mask)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
-
-        return mlm_loss, scores
-
-    
-class BertWithMoMoShareCommonAttnLargeNew(nn.Module):
-    def __init__(self, config, centers):
-        super(BertWithMoMoShareCommonAttnLargeNew, self).__init__()
-        self.config = config        
-        self.bert = BertMoMoShareCommonAttnLargeNewModel(config, centers)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def forward(self, input_ids, attention_mask, labels):
-        output = self.bert(input_ids, attention_mask)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
-
-        return mlm_loss, scores
- 
-    
-class BertWithMoMoDynamicCenter(nn.Module):
-    def __init__(self, config):
-        super(BertWithMoMoDynamicCenter, self).__init__()
-        self.config = config        
-        self.bert = BertMoMoDynamicCenterModel(config)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def forward(self, input_ids, attention_mask, labels):
-        output = self.bert(input_ids, attention_mask)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
-
-        return mlm_loss, scores
-    
-    
-class BertWithMoMoDynamicDistribution(nn.Module):
-    def __init__(self, config):
-        super(BertWithMoMoDynamicDistribution, self).__init__()
-        self.config = config        
-        self.bert = BertMoMoDynamicDistributionModel(config)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-
-    def forward(self, input_ids, attention_mask, labels):
-        output = self.bert(input_ids, attention_mask)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
-
-        return mlm_loss, scores
-    
-    
 class BertWithMoMoModelRouter(nn.Module):
     def __init__(self, config, centers):
         super(BertWithMoMoModelRouter, self).__init__()
@@ -275,38 +109,7 @@ class BertWithMoMoModelRouter(nn.Module):
         mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
 
         return mlm_loss, scores
-    
-    
-class BertWithMoMoModelRouterCommonLinGate(nn.Module):
-    def __init__(self, config, centers):
-        super(BertWithMoMoModelRouterCommonLinGate, self).__init__()
-        self.config = config        
-        self.bert = BertMoMoModelRouterCommonLinGateModel(config, centers)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def forward(self, input_ids, attention_mask, labels, routing_states):
-        output = self.bert(input_ids, attention_mask, routing_states)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
-
-        return mlm_loss, scores
-    
-    
+ 
 class BertWithMoMoModelRouterCommonAttnLargeNew(nn.Module):
     def __init__(self, config, centers):
         super(BertWithMoMoModelRouterCommonAttnLargeNew, self).__init__()
@@ -425,65 +228,7 @@ class BertWithMoMoEncoderGating(nn.Module):
         mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)
 
         return mlm_loss, scores
-    
-    
-class BertWithMoMoEncoderGatingMCR(nn.Module):
-    def __init__(self, config):
-        super(BertWithMoMoEncoderGatingMCR, self).__init__()
-        self.config = config        
-        self.bert = BertMoMoEncoderGatingMCRModel(config)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def forward(self, input_ids, attention_mask, labels):
-        output, mcr_loss = self.bert(input_ids, attention_mask)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)        
-        return mlm_loss, mcr_loss, scores
-    
-    
-class BertWithMoMoAutoEncoderGating(nn.Module):
-    def __init__(self, config):
-        super(BertWithMoMoAutoEncoderGating, self).__init__()
-        self.config = config        
-        self.bert = BertMoMoAutoEncoderGatingModel(config)
-        self.head = BertOnlyMLMHead(config)
-        self.criterion = nn.CrossEntropyLoss() 
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def forward(self, input_ids, attention_mask, labels):
-        output, mse_loss = self.bert(input_ids, attention_mask)
-        scores = self.head(output)
-        mlm_loss = self.criterion(scores.view(-1, self.config.vocab_size), labels.view(-1)) # scores should be of size (num_words, vocab_size)        
-        return mlm_loss, mse_loss, scores
-    
+ 
     
 class BertWithMoMoT(nn.Module):
     def __init__(self, config, centers):
